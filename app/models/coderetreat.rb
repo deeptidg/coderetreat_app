d71213017aa224732f1366d72d5100e215766c2c
deeptig <deeptig@Maheshs-MacBook-2.local>
2014-09-25 16:39:06 -0400

    Push knowledge of stauses from edit view into model

--- app/models/coderetreat.rb
+++ app/models/coderetreat.rb
@@ -6,6 +6,10 @@ class Coderetreat < ActiveRecord::Base
     all
   end
 
+  def available_next_statuses
+    ["not_started", "in_session", "on_break"] - [self.status]
+  end
+
   def update_status(new_status)
     puts "The Coderetreat object receives new_status #{new_status}"
     self.update_attributes status: new_status
